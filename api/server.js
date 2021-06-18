fcd6c5e3f993cdece94c17366f01249aa07e893c
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-06-18 12:23:12 -0600

    finished, and all my tests are passing

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
-const express = require("express")
-
-const server = express()
+const express = require("express");
+const carRouter = require("./cars/cars-router.js");
+const server = express();
 
 // DO YOUR MAGIC
+server.use(express.json());
+server.use("/api/cars", carRouter);
 
-module.exports = server
+server.get("*", (req, res) => {
+  res.send(`<h2>Sorry Natalie Portman is not here</h2>`);
+});
+
+module.exports = server;
